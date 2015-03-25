e58a63c28cef8753719f8642dc52575c7beb262f
sergei.sergeev <sergeev.srg@gmail.com>
2015-03-25 12:54:44 +0300

    Updated base list item class

--- js/src/Base/BaseListItem.js
+++ js/src/Base/BaseListItem.js
@@ -4,20 +4,22 @@ SPListRepo.BaseListItem =
 	
 	function BaseListItem(item) {
 		var e = Function.validateParameters(arguments, [
-			{ name: "item", type: SP.ListItem }
+			{ name: "item", type: SP.ListItem, optional:true }
 		], true);
 
 		if (e) throw e;
 
-		this.item = item;
-		this.id = item.get_id();
-		this.created = this.getFieldValue(SPListRepo.Fields.Created);
-		this.createdBy = this.getFieldValue(SPListRepo.Fields.CreatedBy);
-		this.modified = this.getFieldValue(SPListRepo.Fields.Modified);
-		this.modifiedBy = this.getFieldValue(SPListRepo.Fields.ModifiedBy);
-		this.title = this.getFieldValue(SPListRepo.Fields.Title);
-		this.fileDirRef = this.getFieldValue(SPListRepo.Fields.FileDirRef);
-		this.fileSystemObjectType = this.item.get_fileSystemObjectType();
+		if(item) {
+			this.item = item;
+			this.id = item.get_id();
+			this.created = this.getFieldValue(SPListRepo.Fields.Created);
+			this.createdBy = this.getFieldValue(SPListRepo.Fields.CreatedBy);
+			this.modified = this.getFieldValue(SPListRepo.Fields.Modified);
+			this.modifiedBy = this.getFieldValue(SPListRepo.Fields.ModifiedBy);
+			this.title = this.getFieldValue(SPListRepo.Fields.Title);
+			this.fileDirRef = this.getFieldValue(SPListRepo.Fields.FileDirRef);
+			this.fileSystemObjectType = this.item.get_fileSystemObjectType();
+		}
 	}
 	
 	BaseListItem.prototype.getFieldValue = function(name){

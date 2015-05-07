d2427959254f50cf766736d05ae52398835f0808
sergei_sergeev <sergeev.srg@gmail.com>
2015-05-07 22:38:54 +0300

    Added support for web.getList method. REST version left for backward capability (or for PRE Feb.2015 CU for on premises).

--- js/build/release/sp.list.repository.js
+++ js/build/release/sp.list.repository.js
@@ -56,34 +56,49 @@ SPListRepo.RequestError.registerClass("SPListRepo.RequestError");
 
 SPListRepo.ListService = function($) {
     "use strict";
+    function getListUsingRest(url, success, error) {
+        $.ajax({
+            url: url,
+            type: "GET",
+            contentType: "application/json;odata=verbose",
+            headers: {
+                Accept: "application/json;odata=verbose"
+            },
+            success: function(data) {
+                var context = SP.ClientContext.get_current(), list = context.get_web().get_lists().getById(data.d.results[0].Id);
+                context.load(list);
+                context.executeQueryAsync(function() {
+                    success(list);
+                }, function(sender, error) {
+                    loadDeferred.reject(new SPListRepo.RequestError(error));
+                });
+            },
+            error: function(jqXHR, textStatus) {
+                error(textStatus);
+            }
+        });
+    }
     var listsPool = {};
     return {
         getListByUrl: function(listUrl) {
             if (listsPool[listUrl]) return listsPool[listUrl];
             var loadDeferred = $.Deferred();
             listsPool[listUrl] = loadDeferred;
-            var webAbsoluteUrl = SPListRepo.Helpers.ensureTrailingSlash(_spPageContextInfo.webAbsoluteUrl), webServerRelativeUrl = SPListRepo.Helpers.ensureTrailingSlash(_spPageContextInfo.webServerRelativeUrl), url = String.format("{0}_api/web/lists/?$expand=RootFolder&$filter=RootFolder/ServerRelativeUrl eq '{1}{2}'&$select=ID", webAbsoluteUrl, webServerRelativeUrl, listUrl);
-            $.ajax({
-                url: url,
-                type: "GET",
-                contentType: "application/json;odata=verbose",
-                headers: {
-                    Accept: "application/json;odata=verbose"
-                },
-                success: function(data) {
-                    var context = SP.ClientContext.get_current(), list = context.get_web().get_lists().getById(data.d.results[0].Id);
-                    context.load(list);
-                    context.executeQueryAsync(function() {
-                        listsPool[listUrl] = list;
-                        loadDeferred.resolve(list);
-                    }, function(sender, error) {
-                        loadDeferred.reject(new SPListRepo.RequestError(error));
-                    });
-                },
-                error: function(jqXHR, textStatus) {
-                    loadDeferred.reject(new SPListRepo.RequestError(textStatus));
-                }
-            });
+            var webAbsoluteUrl = SPListRepo.Helpers.ensureTrailingSlash(_spPageContextInfo.webAbsoluteUrl), webServerRelativeUrl = SPListRepo.Helpers.ensureTrailingSlash(_spPageContextInfo.webServerRelativeUrl), url = String.format("{0}_api/web/lists/?$expand=RootFolder&$filter=RootFolder/ServerRelativeUrl eq '{1}{2}'&$select=ID", webAbsoluteUrl, webServerRelativeUrl, listUrl), context = SP.ClientContext.get_current(), success = function(list) {
+                listsPool[listUrl] = list;
+                loadDeferred.resolve(list);
+            }, error = function(err) {
+                loadDeferred.reject(new SPListRepo.RequestError(err));
+            };
+            if (context.get_web().getList) {
+                var list = context.get_web().getList(String.format("{0}{1}", webServerRelativeUrl, listUrl));
+                context.load(list);
+                context.executeQueryAsync(function() {
+                    success(list);
+                }, function(sender, err) {
+                    error(err);
+                });
+            } else getListUsingRest(url, success, error);
             return loadDeferred.promise();
         }
     };

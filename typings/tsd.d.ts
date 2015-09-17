dba254a6962edd6db5f57240d06c6152a6614802
sergei_sergeev <sergeev.srg@gmail.com>
2015-09-18 00:10:58 +0300

    Added typings.

---
+++ typings/tsd.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="camljs/camljs.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="microsoft-ajax/microsoft.ajax.d.ts" />
+/// <reference path="sharepoint/SharePoint.d.ts" />
